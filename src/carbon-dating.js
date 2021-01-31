e21176ec198d12441aaa33d6ed4a0f655a2eb0cf
Igor Lyatskiy <igor.lyatskiy@gmail.com>
2021-02-01 00:26:42 +0300

    feat: finished task with cats and carbon-dating, started dream-team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+	if (typeof sampleActivity != "string" || typeof sampleActivity == "NaN")
+		return false;
+	if (!isNaN(sampleActivity)) return false;
+	if (
+		Math.log2(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD < 0 ||
+		Math.log2(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD ==
+			Infinity ||
+		isNaN(Math.log2(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD)
+	)
+		return false;
+	return Math.log2(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	let array = [];
+	let j = 0;
+	if (typeof members == "null" || typeof members == "undefined") return false;
+	for (let i = 0; i < members.length; i++) {
+		if (typeof members[i] == "string" && members[i].toUpperCase != members[i]) {
+			while (members[i][j] == " ") {
+				j++;
+			}
+
+			array.push(members[i][j]);
+		}
+	}
+	array = array.sort();
+	return array.join("");
 };
