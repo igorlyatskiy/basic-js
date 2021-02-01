805368e6cf515e219bd6f8bdb8b90982cba02913
Igor Lyatskiy <igor.lyatskiy@gmail.com>
2021-02-01 16:28:03 +0300

    fix: deleted some wrong strings

--- src/what-season.js
+++ src/what-season.js
@@ -1,10 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-	if (!date) return "Unable to determine the time of year!";
+	if (!date || isNaN(date)) return "Unable to determine the time of year!";
 	else {
-		let time = date.toDateString();
-		time = date.getMonth();
+		let time = date.getMonth();
 		if (time >= 2 && time <= 4) return "spring";
 		else if (time >= 5 && time <= 7) return "summer";
 		else if (time >= 8 && time <= 10) return "autumn";
