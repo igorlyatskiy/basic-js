1a8b56d6a8720b3119e431087b315b43dd900a39
Igor Lyatskiy <igor.lyatskiy@gmail.com>
2021-02-01 14:00:51 +0300

    feat: 3 wrong transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+	if (!Array.isArray(arr)) throw Error;
+	let newArray = arr;
+	for (let i = 0; i < newArray.length; i++) {
+		if (typeof newArray[i] == "object") {
+			if (i > 0) newArray.splice(i - 1, 1);
+			else newArray.shift();
+		}
+		if (newArray[i] == "--discard-next") {
+			if (
+				newArray[i + 2] == "--discard-prev" ||
+				newArray[i + 2] == "--double-prev"
+			) {
+				newArray.splice(i + 1, 1);
+			}
+			if (i <= newArray.length - 2) {
+				newArray.splice(i, 1);
+			}
+			if (i == newArray.length - 1) newArray.pop();
+		}
+		if (newArray[i] == "--discard-prev") {
+			if (i > 1) {
+				newArray.splice(i - 2, 2);
+			} else if (i == 0) {
+				newArray.shift();
+			} else if (i == 1) {
+				newArray.splice(i - 1, 1);
+				newArray.shift();
+			}
+		}
+		/////////////////////////////////////////
+		if (newArray[i] == "--double-next") {
+			if (i <= newArray.length - 2) {
+				delete newArray[i];
+				newArray[i] = newArray[i + 1];
+			} else newArray.pop();
+		}
+		if (newArray[i] == "--double-prev") {
+			if (i > 0) {
+				delete newArray[i];
+				newArray[i] = newArray[i - 1];
+			} else newArray.shift();
+		}
+	}
+	return newArray;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+	if (+date == Infinity) throw Error;
+	if (typeof date != "string") throw Error;
+	if (date == undefined || date == null)
+		return "Unable to determine the time of year!";
+	let newArray = date.split("");
+	if (+newArray[1] >= 12 || newArray[1] <= 2) return "winter";
+	if (+newArray[1] >= 3 && newArray[1] <= 5) return "spring";
+	if (+newArray[1] >= 6 && newArray[1] <= 8) return "summer";
+	if (+newArray[1] >= 9 && newArray[1] <= 11) return "autumn";
 };
